a92c41715637c1741d75d5221df9fb7e847323be
Omkar Baburao Kamble <121740559+okamble605@users.noreply.github.com>
2024-12-19 18:36:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-//complete this code
-class Rectangle {}
+// Define the Rectangle class
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  // Getter for width
+  get width() {
+    return this._width;
+  }
+
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate area
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+// Define the Square class extending Rectangle
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); // Call the Rectangle constructor with side for both width and height
+  }
+
+  // Method to calculate perimeter
+  getPerimeter() {
+    return 4 * this.width; // Since width and height are the same for a square
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
